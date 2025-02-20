6410a23e11871fe1de6f319f837feb6803ab2a69
algirdas.tamasauskas <algirdas.tamasauskas@visma.com>
2025-02-20 09:19:53 +0200

    Update deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  },
   markdown: {
     shikiConfig: {
       theme: 'dracula',
       wrap: true
+    },
+    legacy: {
+      collections: true,
+      slug: true,
     }
   }
 });
